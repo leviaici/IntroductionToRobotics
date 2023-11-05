de8fac39374e8a0e24eb3e37ec5819b8325f8e0b
Adrian-George Levențiu <92024800+leviaici@users.noreply.github.com>
2023-11-05 11:21:05 +0200

    Homework 3

--- Homework_3/homework-3.ino
+++ Homework_3/homework-3.ino
@@ -71,6 +71,7 @@ int path[segmentLength][directions] = {
   NA, NA, C, NA, // dp
 };
 
+// states of the cursor
 enum State {
   IDLE,
   MOVING_UP,
@@ -100,36 +101,36 @@ void loop() {
       xValue = analogRead(pinX);
       yValue = analogRead(pinY);
 
-      if (currentTime - startTime >= blinkInterval) {
+      if (currentTime - startTime >= blinkInterval) { // blinking state of the cursor
         blinkState = !blinkState;
         digitalWrite(segmentPins[index], blinkState);
         startTime = currentTime;
       }
 
-      if (xValue + joystickSensitivity < minimumThreshold) {
-        verifyJoystickMotion(LEFT);
+      if (xValue + joystickSensitivity < minimumThreshold) { // verifying the joystick motion for x value
+        verifyJoystickMotion(LEFT);                          // and the fact that the joystick could go in the desired direction
       } else if (xValue - joystickSensitivity > maximumThreshold) {
         verifyJoystickMotion(RIGHT);
-      } else if (yValue + joystickSensitivity < minimumThreshold) {
-        verifyJoystickMotion(DOWN);
+      } else if (yValue + joystickSensitivity < minimumThreshold) { // verifying the joystick motion for y value
+        verifyJoystickMotion(DOWN);                                 // and the fact that the joystick could go in the desired direction
       } else if (yValue - joystickSensitivity > maximumThreshold) {
         verifyJoystickMotion(UP);
       } else joystickMoved = false;
 
       break;
-    case MOVING_UP:
+    case MOVING_UP: // the cursor is going up
       updateCase(UP);
 
       break;
-    case MOVING_DOWN:
+    case MOVING_DOWN: // the cursor is going down
       updateCase(DOWN);
 
       break;
-    case MOVING_LEFT:
+    case MOVING_LEFT: // the cursor is going left
       updateCase(LEFT);
 
       break;
-    case MOVING_RIGHT:
+    case MOVING_RIGHT: // the cursor is going right
       updateCase(RIGHT);
     
       break;
@@ -137,8 +138,8 @@ void loop() {
 }
 
 void verifyJoystickMotion(int state) {
-  if (!joystickMoved)
-    if (path[index][state] != NA) {
+  if (!joystickMoved) // verifying if it's not true so it won't move more than 1 place at a time
+    if (path[index][state] != NA) { // verifying that it can go to that path
       if (state == UP)
         gameState = MOVING_UP;
       else if (state == DOWN)
@@ -147,23 +148,23 @@ void verifyJoystickMotion(int state) {
         gameState = MOVING_LEFT;
       else if (state == RIGHT)
         gameState = MOVING_RIGHT;
-      joystickMoved = true;
+      joystickMoved = true; // setting it true so it won't move more than 1 place at a time
     }
 }
 
 void resetGame() {
   for (int i = 0; i < segmentLength; i++) {
         segmentStates[i] = LOW;
-        digitalWrite(segmentPins[i], segmentStates[i]);
+        digitalWrite(segmentPins[i], segmentStates[i]); // resetting all 7-seg LEDs
       }
-      index = 7;
-      gameState = IDLE;
+      index = 7; // putting back the cursor on the dp
+      gameState = IDLE; // resetting the state
 }
 
 void updateCase(int nextState) {
-  digitalWrite(segmentPins[index], segmentStates[index]);
-  index = path[index][nextState];
-  startTime = currentTime;
+  digitalWrite(segmentPins[index], segmentStates[index]); // we are either lighting up or turning off the LED we are currently on and preparing to leave
+  index = path[index][nextState]; // modifying the cursor to the next one
+  startTime = currentTime; // modifying startTime for the blinking
   gameState = IDLE;
 }
 
@@ -172,17 +173,17 @@ void changeState() {
   swState = digitalRead(pinSW);
 
   if (!swState) {
-    if (!buttonPressed) {
-      buttonPressedTime = millis();
-      buttonPressed = true;
-    } else if (currentTime - buttonPressedTime >= longPressDuration) {
+    if (!buttonPressed) { // verifying if the button was pressed
+      buttonPressedTime = millis(); // starting recording the time when the button was first pressed
+      buttonPressed = true; // saving the fact that it was pressed
+    } else if (currentTime - buttonPressedTime >= longPressDuration) { // verifying if it's enough pressed so it can reset and that the button was pressed
       resetGame();
-      buttonPressed = false;
+      buttonPressed = false; // resetting the value
     }
   } else {
-    if (buttonPressed && currentTime - buttonPressedTime >= swDebounceDelay) {
+    if (buttonPressed && currentTime - buttonPressedTime >= swDebounceDelay) { // if it's not a long press, it will just !state of the LED
       segmentStates[index] = !segmentStates[index];
     }
-    buttonPressed = false;
+    buttonPressed = false; // resetting the value for the next press
   }
 }
